f965e836f965c70417bfddc0eae04ed46edb3b7c
Herandom <46648268+Herandom@users.noreply.github.com>
2024-08-22 23:06:19 +0200

    highlightgroup for yank changed, now less horrible

--- init.lua
+++ init.lua
@@ -199,7 +199,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { higroup = 'DiagnosticVirtualTextInfo', timeout = 200 }
   end,
 })
 
